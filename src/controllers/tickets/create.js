11928df162cc0f937cfa6f3e20138df4db144b9d
Leandro <leandromkt.tr@gmail.com>
2026-06-05 19:53:01 -0300

    Criando o objeto de ticket

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,3 +1,18 @@
+import {randomUUID} from 'node:crypto';
+
 export function create ({request, response}) {
-    return response.end("Criado com sucesso!");
+    const {equipament, description, username} = request.body;
+
+
+    const ticket = {
+        id: randomUUID(),
+        equipament,
+        description,
+        username,
+        status: 'open',
+        created_at: new Date(),
+        up_dated_at: new Date(),
+    }
+
+    return response.end(JSON.stringify(ticket));
 }
